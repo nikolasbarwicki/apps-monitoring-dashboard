403135092d059343793cd9739b1f08f5db271a9d
Nikolas Barwicki <nikolas.barwicki@gmail.com>
2022-05-06 15:13:45 +0200

    feat: add link to the main page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from 'react'
+import Link from 'next/link'
 
 interface LayoutProps {
   children?: ReactNode
@@ -6,6 +7,13 @@ interface LayoutProps {
 
 export const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <main className="container mx-auto max-w-7xl px-8">{children}</main>
+    <main className="container mx-auto max-w-7xl px-8">
+      <Link href="/" passHref>
+        <a>
+          <h1 className="py-8 text-2xl font-medium">Dashboard Monitor</h1>
+        </a>
+      </Link>
+      {children}
+    </main>
   )
 }
